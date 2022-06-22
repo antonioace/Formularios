d88e44735dcf5eedf6efa033f8cf8402ff1adde0
Antonio <antonioacevedocastellanos@gmail.com>
2022-06-21 23:28:09 -0500

    Desarrollando forms reactivoes

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -8,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+
+
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [, [Validators.required, Validators.minLength(3)],],
+    favoritos: this.fb.array([['MegaDeth'], ['Fiesta Pagana'], ['Iron']], [Validators.required])
+
+  })
+
+
+  get FavoritosArr() {
+
+
+    return this.miFormulario.get('favoritos') as FormArray
+  }
+  validarNombre() {
+
+    return this.miFormulario.controls['nombre'].errors && this.miFormulario.controls['nombre'].touched
+  }
+
+
+  guardar() {
+
+
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched()
+      return;
+    }
+    console.log('Esta entrando por aca')
+
+  }
+
 
   ngOnInit(): void {
   }
+
 
 }
